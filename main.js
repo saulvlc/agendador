6de251232f71010e7c42ca58602bfe32fcf8b1e5
saulvlc <86716982+saulvlc@users.noreply.github.com>
2023-03-23 12:21:33 +0100

    Select day

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 let clientId = '3MVG9LBJLApeX_PAlM3ly65t6lr9hb3kGRcf3RGayIHLd8dh8EAX0zDhkfpIbJb.m9BGhzLbWTLoFt1qfPPQq';
-//let clientId = '3MVG9t0sl2P.pByqmqBfMeoWxc0JvuM.VK0cYxRTt7y_jsbDr9qYjciTlYho0HsJm64kbl5P0CsAN8gBYoCL9';
 let clientSecret = '62C6A540AE04A830261E32DDC5E3462AD017FE9115F95F5455765E982099003A';
-//let clientSecret ='D5D3E335E78E0659B05A931D2280E9A9ED60C99AEF50BD048684209783031D7A'
 let username = 'anebrera@sectorpublicospring20.demo';
 let password = 'Salesforce1234';
-//let username = 'mario.de-la-iglesia-caballero-nuzz@force.com';
-//let password = 'Salesforce23$';
 let url = 'https://login.salesforce.com/services/oauth2/token';
-const prueba = new Array();
+let eventos = new Array();
 
 // Hacemos la llamada  para obtener el token de acceson y ponemos "Accept": "application/json","Access-Control-Allow-Origin": "*","Access-Control-Allow-Methods": "*"
 try {
@@ -85,8 +81,8 @@ function showUsers() {
 function cambiaVendedor() {
     let idVendedor = document.getElementById("vendedor").value;
     console.log("idVendedor: " + idVendedor);
-    let nombreVendedor = document.getElementById("vendedor").options[document.getElementById("vendedor").selectedIndex].text;
-    console.log("nombreVendedor: " + nombreVendedor);
+    window.nombreVendedor = document.getElementById("vendedor").options[document.getElementById("vendedor").selectedIndex].text;
+    console.log("nombreVendedor: " + window.nombreVendedor);
     //Hacemos la consulta para obtener todos los eventos
     fetch(window.instanceUrl + '/services/data/v57.0/sobjects/Event', {
         method: 'GET',
@@ -94,83 +90,150 @@ function cambiaVendedor() {
             'Authorization': 'Bearer ' + window.token
         }
     }).then(response => {
-        //Mostramos por consola la respuesta
-        console.log(response);
-        return response.json();
-        
+        // Convertimos el objeto Response a un objeto JSON
+        let data = response.json();
+        // Devolvemos el objeto JSON
+        return data;        
     })
         .then(data => {
-            //Guardamos los eventos en una variable global
-            window.event = data.records;
-            console.log("Fechas: "+data.records);
-            //LLamamos a la función para hacer una llamada al evento del vendedor
-            getEventVendedor(idVendedor);
+            //Envimos los datos a la función que los procesa
+            getEventVendedor(idVendedor , data);
         });
 }
 
-function getEventVendedor(idVendedor) {
-    //Recorremos los eventos
-    window.event.forEach(event => {
-        //Si el evento es del vendedor
-        if (event.OwnerId == idVendedor) {
-            //Guardamos el evento en una variable global
-            window.eventVendedor = event;
-            //Llamamos a la función que muestra las fechas y horas
-            showDates();
-        }
+function getEventVendedor(idVendedor, data) {
+    eventos = [];
+    //Recorremos data que es un objet object
+    data.recentItems.forEach(event => {
+        let urlEvent = "https://sectorpublicospring20.my.salesforce.com" + event.attributes.url;
+        //Hacemos la consulta para obtener el evento
+        fetch(urlEvent, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + window.token
+            }
+        }).then(response => {
+            //Mostramos por consola la respuesta
+            // Recibimos un object Response de la petición fetch()
+            // Convertimos el objeto Response a un objeto JSON
+            let datos = response.json();
+            // Devolvemos el objeto JSON
+            return datos;
+        })
+            .then(datos => {
+                if(datos.OwnerId == idVendedor){
+                    //Guardamos en un array de objetos los datos de las reservas
+                    let Subject = datos.Subject;
+                    let ActivityDate = datos.ActivityDate;
+                    let EndDate = datos.EndDate;
+                    let evento = {
+                        Subject: Subject,
+                        ActivityDate: ActivityDate,
+                        EndDate: EndDate
+                    }
+                    eventos.push(evento);      
+                }
+            }).finally(() => {
+                if(eventos.length == 0){
+                    let noReservas = "No hay reservas para este vendedor";
+                    document.getElementById("proximaCita").innerHTML = noReservas;
+                }else{
+                    proximasCitas();
+                    mostrarEnCalendario();
+                }
+            });
     });
+    
+    
+    
 }
 
+function proximasCitas() {
+    let proximaCita = document.getElementById("proximaCita");
+    let horas = document.getElementById("horas");
+    proximaCita.innerHTML = "";
+    horas.innerHTML = "";
+    //Ordenamo los eventos por fecha
+    eventos.sort((a, b) => {
+        if (a.ActivityDate > b.ActivityDate) {
+            return 1;
+        }
+        if (a.ActivityDate < b.ActivityDate) {
+            return -1;
+        }
+        return 0;
+    });
+// Obtenemos la fecha de la proxima cita a partir de la fecha actual
+    let fechaActual = new Date();
+    let fechaActualFormateada = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
+   for(let i = 0; i < eventos.length; i++){
+            let fecha = eventos[i].ActivityDate;
+            let fechaArray = fecha.split("-");
+            let fechaFormateada = fechaArray[2] + "/" + fechaArray[1] + "/" + fechaArray[0];
 
+            if (fechaActualFormateada < fecha) {
+                proximaCita.innerHTML = "Cita reservada más proxima: " + fechaFormateada;
+            }
+            proximaCita.innerHTML = "Cita reservada más proxima: " + fechaFormateada;
+   }
+    
+}
 
-//     let proximaCita = document.getElementById("proximaCita");
-//     let horas = document.getElementById("horas");
-//     proximaCita.innerHTML = "";
-//     horas.innerHTML = "";
-//     let horasDisponiblesText = 'Horas dispobibles: ';
-//     let horasDisponibles = [];
+//     //mostrar en el calendario
+function mostrarEnCalendario(){
+    // Mostramos en la celda del calendario los eventos que tenemos guardado en el array de objetos
+    let calendarEl = document.getElementById('calendar');
+    let fechas = [];
+    for(let i = 0; i < eventos.length; i++){
+        let fecha = eventos[i].ActivityDate;
+        let fechaArray = fecha.split("-");
+        let fechaFormateada = fechaArray[2] + "/" + fechaArray[1] + "/" + fechaArray[0];
+        fechas.push(fechaFormateada);
+    }
+    let calendar = new FullCalendar.Calendar(calendarEl, {});
+    calendar.render();
+    for(let i = 0; i < eventos.length; i++){
+        console.log("aqui"+eventos[i]);
+        calendar.select(eventos[i], eventos[i]);
+    }
+    
+    
+    // let longitud = eventos.length;
+    
+    // let calendar = new FullCalendar.Calendar(calendarEl, {
+    //     initialView: 'dayGridMonth',
+    //     events: 
+        
+    //     [
+    //         {
+    //             title: window.nombreVendedor,
+    //             start: eventos[0].ActivityDate,
+    //             end: eventos[0].EndDate
+    //        },
+    //           {
+    //             title: window.nombreVendedor,
+    //             start: eventos[1].ActivityDate,
+    //             end: eventos[1].EndDate
+    //           },
+    //             {
+    //             title: window.nombreVendedor,
+    //             start: eventos[2].ActivityDate,
+    //             end: eventos[2].EndDate
+    //             }
+              
+    //     ]
+    // });
+    // //Hacemos que cada celda del calendario se pueda hacer click para añadir una cita
+    // calendar.on('dateClick', function (info) {
+    //     console.log('Clicked on: ' + info.dateStr);
+    //     console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+    //     console.log('Current view: ' + info.view.type);
+    //     // change the day's background color just for fun
+    //     info.dayEl.style.backgroundColor = 'red';
+    // });
 
-//     //ponemos la proxima cita del vendedor
-
-//     for (let i = 0; i < listadoVendedor.length; i++) {
-//         if (listadoVendedor[i].id == idVendedor) {
-//             let fecha = listadoVendedor[i].dates[0].date;
-//             let fechaArray = fecha.split("-");
-//             let fechaFormateada = fechaArray[2] + "/" + fechaArray[1] + "/" + fechaArray[0];
-//             proximaCita.innerHTML = "Proxima cita: " + fechaFormateada;
-//             for (let j = 0; j < listadoVendedor[i].dates[0].hours.length; j++) {
-//                 horas.innerHTML += listadoVendedor[i].dates[0].hours[j] + " ";
-//                 //guardamos las horas disponibles en un array
-//                 horasDisponibles.push(listadoVendedor[i].dates[0].hours[j]);
-//             }
-//             horas.innerHTML = horasDisponiblesText + horas.innerHTML;
-//         }
-//     }
-
-//     //mostrar en el calendarioç
-
-//     let calendarEl = document.getElementById('calendar');
-//     let calendar = new FullCalendar.Calendar(calendarEl, {
-//         initialView: 'dayGridMonth',
-//         events: [
-//            //cogemos el nombre del vendedor y las proximas citas y mostramos las horas disponibles
-//             {
-//                 title: nombreVendedor,
-//                 start: listadoVendedor[idVendedor - 1].dates[0].date,
-//                 end: listadoVendedor[idVendedor - 1].dates[0].date,
-//                 color: 'green'
-//             },
-//             {
-//                 title: nombreVendedor,
-//                 start: listadoVendedor[idVendedor - 1].dates[1].date,
-//                 end: listadoVendedor[idVendedor - 1].dates[1].date,
-//                 color: 'green'
-//             }
-//         ]
-//     });
-
-//     calendar.render();
-// }
+    // calendar.render();
+}
 
 
 //git remote add origin https://saulaznarez@dev.azure.com/saulaznarez/agendador/_git/agendador
